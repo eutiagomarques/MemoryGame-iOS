10d3d1cc4abaf38177d3950deb4603aa8d982bde
Tiago Marques <tiago.marques@radixeng.com.br>
2021-07-13 01:44:25 -0300

    criando projeto

---
+++ MemoryCard/MemoryCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryCardApp.swift
+//  MemoryCard
+//
+//  Created by Tiago Marques on 13/07/21.
+//
+
+import SwiftUI
+
+@main
+struct MemoryCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
